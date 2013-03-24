52d5ee3d65f49209553f8e328af47dedce83babf
Michael Kane <kaneplusplus@gmail.com>
2013-03-24 12:50:06 -0400

    Added an option to send an init string when workers are started.

--- cnidaria/start_local_workers.py
+++ cnidaria/start_local_workers.py
@@ -1,7 +1,8 @@
 import os
 
-def start_local_workers(nw=1, key="testkey", host="localhost", port=6379, db=0):
-  rs = str("import argparse, cnidaria\n" +
+def start_local_workers(nw=1, key="testkey", host="localhost", port=6379, 
+  db=0, init_string=''):
+  rs = str("import argparse, cnidaria\n" + init_string + "\n" +
     "w = cnidaria.Worker ('" + key + "', '" + host + "', " + str(port) +
     ", " + str(db) + ")\n" + "w.service()")
   for i in range(nw):
